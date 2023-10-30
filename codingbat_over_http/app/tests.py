da7315045fbd3b7353c082a82e7545cf0f78de2f
Owen <ocotten24@basecampcodingacademy.org>
2023-10-30 13:10:50 -0500

    fixed test cases to match instructions

--- codingbat_over_http/app/tests.py
+++ codingbat_over_http/app/tests.py
@@ -2,58 +2,56 @@ from django.test import SimpleTestCase
 
 
 class TestNearHundred(SimpleTestCase):
-    def test_near_hundred_within_10(self):
-        response = self.client.get("/warmup-1/near-hundred/91")
+    def test_near_hundred_1(self):
+        response = self.client.get("/warmup-1/near-hundred/93")
         self.assertContains(response, True)
 
-    def test_near_hundred_within_10_2(self):
-        response = self.client.get("/warmup-1/near-hundred/109")
+    def test_near_hundred_2(self):
+        response = self.client.get("/warmup-1/near-hundred/90")
         self.assertContains(response, True)
 
-    def test_near_hundred_outside_10(self):
-        response = self.client.get("/warmup-1/near-hundred/1000")
+    def test_near_hundred_3(self):
+        response = self.client.get("/warmup-1/near-hundred/89")
         self.assertContains(response, False)
 
 
 class TestStringSplosion(SimpleTestCase):
-    def test_string_splosion_hello(self):
-        response = self.client.get("/warmup-2/string-splosion/hello")
-        self.assertContains(response, "hhehelhellhello")
+    def test_string_splosion_1(self):
+        response = self.client.get("/warmup-2/string-splosion/Code")
+        self.assertContains(response, "CCoCodCode")
 
-    def test_string_splosion_empty_string(self):
-        response = self.client.get("/warmup-2/string-splosion/testcase")
-        self.assertContains(response, "ttetestesttestctestcatestcastestcase")
+    def test_string_splosion_2(self):
+        response = self.client.get("/warmup-2/string-splosion/abc")
+        self.assertContains(response, "aababc")
 
-    def test_string_splosion_single_character(self):
-        response = self.client.get("/warmup-2/string-splosion/owen")
-        self.assertContains(response, "oowoweowen")
+    def test_string_splosion_3(self):
+        response = self.client.get("/warmup-2/string-splosion/ab")
+        self.assertContains(response, "aab")
 
 
 class TestCatDog(SimpleTestCase):
-    def test_cat_dog_equal_counts(self):
-        response = self.client.get(
-            "/string-2/cat-dog/caaaaaaaaaaaaaaaatdooooooogdogdogdog"
-        )
-        self.assertContains(response, False)
-
-    def test_cat_dog_unequal_counts(self):
-        response = self.client.get("/string-2/cat-dog/cattestcasedog")
+    def test_cat_dog_1(self):
+        response = self.client.get("/string-2/cat-dog/catdog")
         self.assertContains(response, True)
 
-    def test_cat_dog_no_cats_or_dogs(self):
-        response = self.client.get("/string-2/cat-dog/catdogdogdogdogcatcatcat")
+    def test_cat_dog_2(self):
+        response = self.client.get("/string-2/cat-dog/catcat")
+        self.assertContains(response, False)
+
+    def test_cat_dog_3(self):
+        response = self.client.get("/string-2/cat-dog/1cat1cadodog")
         self.assertContains(response, True)
 
 
 class TestLoneSum(SimpleTestCase):
-    def test_lone_sum_no_repeated_values(self):
-        response = self.client.get("/logic-2/lone-sum/1/7/3")
-        self.assertContains(response, 11)
+    def test_lone_sum_1(self):
+        response = self.client.get("/logic-2/lone-sum/1/2/3")
+        self.assertContains(response, 6)
 
-    def test_lone_sum_one_repeated_value(self):
-        response = self.client.get("/logic-2/lone-sum/3/1/3")
-        self.assertContains(response, 1)
+    def test_lone_sum_2(self):
+        response = self.client.get("/logic-2/lone-sum/3/2/3")
+        self.assertContains(response, 2)
 
-    def test_lone_sum_all_repeated_values(self):
+    def test_lone_sum_3(self):
         response = self.client.get("/logic-2/lone-sum/3/3/3")
         self.assertContains(response, 0)
